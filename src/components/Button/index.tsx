4eb9733a87e661efc17b97f49779aa7885c08be7
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-05-05 09:54:17 -0300

    feat: add onPress to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,11 +4,12 @@ import {Container, Title} from './styles';
 
 interface Props {
   title: string;
+  onPress: () => void;
 }
 
-const Button: React.FC<Props> = ({title}) => {
+const Button: React.FC<Props> = ({title, onPress}) => {
   return (
-    <Container>
+    <Container testID="button" onPress={onPress}>
       <Title>{title}</Title>
     </Container>
   );
