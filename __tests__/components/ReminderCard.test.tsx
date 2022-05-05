0be22a99005157b732eef825c6a9d990a0a7370f
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-05-05 09:08:48 -0300

    test: ensure reminder call onPress when pressed

--- __tests__/components/ReminderCard.test.tsx
+++ __tests__/components/ReminderCard.test.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 
-import {render} from '@testing-library/react-native';
+import {fireEvent, render} from '@testing-library/react-native';
 
 import {ReminderCard} from '../../src/components';
 
 describe('ReminderCard', () => {
   it('should render with correct title', () => {
     const title = 'any_title';
+    const onPress = jest.fn();
 
-    const {getByText} = render(<ReminderCard title={title} />);
+    const {getByText} = render(
+      <ReminderCard title={title} onPress={onPress} />,
+    );
 
     expect(getByText(title)).toBeDefined();
+  });
+
+  it('should call onPress when is pressed', () => {
+    const title = 'any_title';
+    const onPress = jest.fn();
+
+    const {getByTestId} = render(
+      <ReminderCard title={title} onPress={onPress} />,
+    );
+
+    fireEvent.press(getByTestId('reminder_card'));
+
+    expect(onPress).toHaveBeenCalled();
   });
 });
