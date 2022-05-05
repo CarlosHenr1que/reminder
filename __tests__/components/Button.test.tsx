7a133cfdf68cc2a46258426eb73b00fd7ed2d009
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-05-05 09:54:32 -0300

    test: ensure onPress is called when pressed

--- __tests__/components/Button.test.tsx
+++ __tests__/components/Button.test.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
-import {render} from '@testing-library/react-native';
+import {fireEvent, render} from '@testing-library/react-native';
 
 import Button from '../../src/components/Button';
 
 describe('ReminderCard', () => {
   it('should render with correct title', () => {
     const title = 'Novo lembrete';
-    const {getByText} = render(<Button title={title} />);
+    const onPress = jest.fn();
+    const {getByText} = render(<Button title={title} onPress={onPress} />);
 
     expect(getByText(title)).toBeDefined();
+  });
+
+  it('should call onPress when pressed', () => {
+    const title = 'Novo lembrete';
+    const onPress = jest.fn();
+    const {getByTestId} = render(<Button title={title} onPress={onPress} />);
+
+    fireEvent.press(getByTestId('button'));
+
+    expect(onPress).toHaveBeenCalled();
   });
 });
