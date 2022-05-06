8b248b7221dc63344a10bcb3d1618e86949c6829
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-05-06 11:00:34 -0300

    feat: add reminder creation to CreateReminder

--- src/screens/CreateReminder/index.tsx
+++ src/screens/CreateReminder/index.tsx
@@ -1,13 +1,25 @@
 import {useNavigation} from '@react-navigation/core';
 import React, {useState} from 'react';
 import {Button, Input} from '../../components';
+import {useReminder} from '../../hooks/contexts/reminder';
+import {Reminder} from '../../models';
 import {Container, Title} from './styles';
 
 const CreateReminder: React.FC = () => {
   const [title, setTitle] = useState<string>();
+  const {addReminders} = useReminder();
   const navigation = useNavigation();
 
   const handleAddReminder = () => {
+    if (!title) {
+      return;
+    }
+    const reminder: Reminder = {
+      id: new Date().toString(),
+      title,
+      done: false,
+    };
+    addReminders([reminder]);
     navigation.navigate('Reminders');
   };
 
