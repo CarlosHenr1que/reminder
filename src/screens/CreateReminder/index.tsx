fc6350d2b62236b3e5af842bea59c3d361dc531e
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-05-06 09:14:33 -0300

    feat: add Input to CreateReminder

--- src/screens/CreateReminder/index.tsx
+++ src/screens/CreateReminder/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import {useNavigation} from '@react-navigation/core';
+import React, {useState} from 'react';
+import {Button, Input} from '../../components';
 import {Container, Title} from './styles';
 
 const CreateReminder: React.FC = () => {
+  const [title, setTitle] = useState<string>();
+  const navigation = useNavigation();
+
+  const handleAddReminder = () => {
+    navigation.navigate('Reminders');
+  };
+
   return (
     <Container>
       <Title>Novo lembrete</Title>
+      <Input
+        value={title}
+        onChange={setTitle}
+        placeholder="Digite o titulo do seu lembrete"
+      />
+      <Button title="Salvar" onPress={handleAddReminder} />
     </Container>
   );
 };
