b3b48f50d6acd58bf98ba06d692ee7b81d5e49c8
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-05-06 10:42:52 -0300

    test: ensure reminder will be added with current reminders

--- __mocks__/Reminder/index.ts
+++ __mocks__/Reminder/index.ts
@@ -7,6 +7,11 @@ export default class ReminderBuilder {
     title: 'any_title',
   };
 
+  public setId(id: string) {
+    this.reminder.id = id;
+    return this;
+  }
+
   public build() {
     const reminderClone = this.reminder;
     return reminderClone;
--- __tests__/hooks/contexts/reminder.test.tsx
+++ __tests__/hooks/contexts/reminder.test.tsx
@@ -7,16 +7,18 @@ import ReminderBuilder from '../../../__mocks__/Reminder';
 
 describe('Reminder hook', () => {
   it('should be able to save a reminder', () => {
-    const reminder = new ReminderBuilder().build();
-    const reminders = [reminder];
+    const defaultReminder = new ReminderBuilder().build();
+    const reminder = new ReminderBuilder().setId('any_other_id').build();
+    const reminders = [defaultReminder];
 
     const {result} = renderHook(() => useReminder(), {
       wrapper: ReminderProvider,
     });
 
     act(() => result.current.addReminders(reminders));
+    act(() => result.current.addReminders([reminder]));
 
-    expect(result.current.data.reminders).toEqual(reminders);
+    expect(result.current.data.reminders).toEqual([...reminders, reminder]);
   });
 
   it('should mark reminder as done when doneReminder is called', () => {
