23718f5452b2d8a277b132c6678167c176f3e290
Carlos Borges <carlosh.mb@outlook.com>
2024-03-27 21:45:04 -0300

    style: change reminders screen text colors

--- src/screens/Reminders/styles.ts
+++ src/screens/Reminders/styles.ts
@@ -2,13 +2,13 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #2b2b2b;
+  background-color: #171717;
   padding: 60px 16px 0px 16px;
 `;
 
 export const Title = styled.Text`
   font-size: 32px;
   font-weight: bold;
-  color: #7bc8e0;
+  color: #ffffff;
   margin-bottom: 30px;
 `;
